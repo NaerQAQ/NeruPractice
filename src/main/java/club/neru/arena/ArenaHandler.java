a010ded9aec4b8c923c267f26e5c229d69a67312
NaerQAQ <134604359+NaerQAQ@users.noreply.github.com>
2023-10-09 19:57:08 +0800

    Javadoc 补充。

--- src/main/java/club/neru/arena/ArenaHandler.java
+++ src/main/java/club/neru/arena/ArenaHandler.java
@@ -2,11 +2,27 @@ package club.neru.arena;
 
 import club.neru.NeruPractice;
 
+/**
+ * 竞技场处理程序。
+ *
+ * @author NaerQAQ
+ * @version 1.0
+ * @since 2023/10/9
+ */
 public class ArenaHandler {
+    /**
+     * 序列化后 Json 字符串所在的键值。
+     */
     public static final String ARENA_JSON_KEY = "arena";
 
+    /**
+     * 竞技场路径。
+     */
     public static final String ARENA_PATH =
             NeruPractice.getDataFolderAbsolutePath() + "/arena";
 
+    /**
+     * 母竞技场所在的路径。
+     */
     public static final String ARENA_PARENT_PATH = ARENA_PATH + "/parent";
 }
