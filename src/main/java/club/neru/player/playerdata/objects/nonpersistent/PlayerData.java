0e4b385b690686acd072382ced8033da0906e064
NaerQAQ <134604359+NaerQAQ@users.noreply.github.com>
2023-10-14 05:45:08 +0800

    小添加。

--- src/main/java/club/neru/player/playerdata/objects/nonpersistent/PlayerData.java
+++ src/main/java/club/neru/player/playerdata/objects/nonpersistent/PlayerData.java
@@ -3,9 +3,11 @@ package club.neru.player.playerdata.objects.nonpersistent;
 import club.neru.match.interfaces.MatchInterface;
 import club.neru.player.playerdata.enums.PlayerState;
 import club.neru.player.playerdata.objects.persistent.SettingsData;
+import fr.mrmicky.fastboard.FastBoard;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.bukkit.scoreboard.Scoreboard;
 
 /**
  * 非持久化存储的玩家数据对象。
@@ -32,6 +34,16 @@ public class PlayerData {
      * 玩家比赛对象。
      */
     private MatchInterface matchInterface;
+
+    /**
+     * 玩家计分板对象。
+     */
+    private Scoreboard scoreboard;
+
+    /**
+     * {@link FastBoard} 对象。
+     */
+    private FastBoard fastBoard;
 
     /**
      * 是否在水中。
